c6e3b70c81872d85aae82b6f4671820f1875ae5a
Taavi Tammaru <taavi1p@gmail.com>
2025-09-03 02:21:43 +0300

    mobile version working, loading animation

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,5 +1,11 @@
 import React from "react";
+import "./Loader.css";
 
-const Loader = () => <div>Loading...</div>;
+const Loader = () => (
+  <div className="loader-container">
+    <div className="loader-spinner"></div>
+    <div className="loader-text">Loading...</div>
+  </div>
+);
 
 export default Loader;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -17,14 +17,21 @@ const Navbar = () => {
         <Link to="/hydro" className="nav-link">Hydro</Link>
       </div>
       {/* Hamburger Icon for Mobile */}
-      <div className="hamburger mobile" onClick={() => setMenuOpen(!menuOpen)}>
+      <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
         <span className="bar"></span>
         <span className="bar"></span>
         <span className="bar"></span>
       </div>
       {/* Mobile Menu */}
       {menuOpen && (
-        <div className="mobile-menu mobile">
+        <div className="mobile-menu">
+          <button
+            className="close-menu-btn"
+            onClick={() => setMenuOpen(false)}
+            aria-label="Close menu"
+          >
+            &times;
+          </button>
           <Link to="/" className="nav-link" onClick={() => setMenuOpen(false)}>Home</Link>
           <Link to="/nuclear" className="nav-link" onClick={() => setMenuOpen(false)}>Nuclear</Link>
           <Link to="/solar" className="nav-link" onClick={() => setMenuOpen(false)}>Solar</Link>
